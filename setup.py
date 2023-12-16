5adba59c888dcb55e3fc8d52e3cad4b0daaaaa29
strawberryhacker <bjorn.thoralf@outlook.com>
2023-12-16 14:00:19 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name = 'spritz-cipher', version = '0.0', description = 'spritz cipher implementation', packages = find_packages(), zip_safe = False)
