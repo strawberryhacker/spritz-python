4c485df89f3808527b6c3b114b51dff809124d3c
strawberryhacker <bjorn.thoralf@outlook.com>
2023-12-16 16:25:43 +0000

    .

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name = 'spritz', version = '0.0', description = 'spritz cipher implementation', packages = ['spritz'], zip_safe = False)
+setup(name = 'spritz', version = '0.1', description = 'spritz cipher implementation', packages = ['spritz'], zip_safe = False)
