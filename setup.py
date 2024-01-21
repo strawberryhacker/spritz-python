d621f68cd6447c9810f698da867900d5bae2ab54
strawberryhacker <bjorn.thoralf@outlook.com>
2024-01-21 21:07:58 +0100

    Version number

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name = 'spritz', version = '1.1', description = 'spritz cipher implementation', packages = ['spritz'], zip_safe = False)
+setup(name = 'spritz', version = '1.2', description = 'spritz cipher implementation', packages = ['spritz'], zip_safe = False)
